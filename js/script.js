ab7edda2e5007937f50bb2bfb2ccfd5f73a3f76b
smkmuhammadiyah1kalasan <smkmuhammadiyah1kalasan@gmail.com>
2026-02-19 14:41:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -346,3 +346,17 @@ beritaImages.forEach(img => {
         img.style.transform = "scale(1)";
     });
 });
+
+/* ===============================
+   HAMBURGER MENU
+================================= */
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
+}
+
